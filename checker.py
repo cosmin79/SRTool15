4f824325bc78d756411e5998e4632489158a2b2d
Cosmin Andrei Carabet <ccarabet@dyn1173-106.wlan.ic.ac.uk>
2015-11-17 17:17:32 +0000

    Multithreaded checker script

--- checker.py
+++ checker.py
@@ -3,34 +3,47 @@ from functools import partial
 import subprocess
 import os
 
-cmd             = ["./srtool"]
+from multiprocessing.pool import ThreadPool
+
 correctSuffix   = "./tests/%s/correct/"
 incorrectSuffix = "./tests/%s/incorrect/"
 
 def appendSuffix(suffix, string) :
   return suffix + string
 
-def runTests(expectedOuput, tests) :
-  failedTests = []
-  for test in tests :
-    cmd.append(test)
-    try :
-      output = subprocess.check_output(cmd)
-    except subprocess.CalledProcessError as exception:
-      output = exception.output
-    result = output.split('\n')[0]
-    if result != expectedOuput :
-      failedTests.append(test)
-    cmd.pop()
-  if len(failedTests) == 0 :
-    print "All tests for", expectedOuput, "passed!!!"
-  else :
-    print len(failedTests), "tests failed for", expectedOuput, "failed!"
+def runTest(expectedOutput, test):
+  cmd = ["./srtool"]
+  cmd.append(test)
+  try :
+    output = subprocess.check_output(cmd)
+  except subprocess.CalledProcessError as exception:
+    output = exception.output
+  result = output.split('\n')[0]
+
+  return (test, result)
+
+def runTests(expectedOutput, tests):
+  results = []
+  pool = ThreadPool(len(tests))
+  for test in tests:
+    results.append(pool.apply_async(runTest, args=(expectedOutput, test)))
+  pool.close()
+  pool.join()
+
+  results = map(lambda future: future.get(), results)
+  failedTests = filter(lambda (test, actual): expectedOutput != actual, results)
+  failedTests = map(lambda (test, actual): test, failedTests)
+
+  if len(failedTests) == 0:
+    print "All tests for %s passed!!!" % (expectedOutput)
+  else:
+    print len(failedTests), "tests failed for %s!" % (expectedOutput)
     print "Following tests failed:"
     for test in failedTests :
-      print test
+      print "Test %s failed\n" % (test)
 
-subprocess.call(["make"]);
+
+subprocess.call(["make"])
 
 correctTests = [correctSuffix % (folder) + file for folder in listdir("tests") if (os.path.isdir("tests/" + folder)) \
                      for file in listdir(correctSuffix % (folder))]
